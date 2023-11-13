ae774e48285f1fa549d90de8ca4dda265dd44728
Rohan Kumar <114764783+rohankumar-1@users.noreply.github.com>
2023-11-12 23:42:18 -0500

    Time: 18 ms (10.14%), Space: 6.2 MB (73.57%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -5,10 +5,9 @@ public:
         
         int i = 0; int j = s.length()-1;
         while(i < j){
-            if(s[i] != s[j]){
+            if(s[i++] != s[j--]){
                 return false;
             }    
-            i++; j--;
         }
 
         return true;
